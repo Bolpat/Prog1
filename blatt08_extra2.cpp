4f24b8db0244312e708a085d2bde3c2929f11b1c
Q. F. Schroll <qs.il.paperinik@gmail.com>
2015-09-22 14:34:08 +0200

    fix.

--- blatt08_extra2.cpp
+++ blatt08_extra2.cpp
@@ -16,7 +16,7 @@ public:
     {
         double result = 0;
         deriv = 0;
-        if (a.empty()) return;
+        if (a.empty()) return result;
         for (unsigned i = a.size() - 1; ; --i)
         {
             result *= x;

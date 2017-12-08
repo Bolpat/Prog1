4a8ea68f79ca827657016d2f79251baf8d2341d1
Q. F. Schroll <qs.il.paperinik@gmail.com>
2017-12-08 02:32:37 +0100

    Update

--- Prog2/WS17/blatt06.cpp
+++ Prog2/WS17/blatt06.cpp
@@ -12,28 +12,35 @@ class Polynom
 {
     struct Monom
     {
-        long a; /// coefficient
-        unsigned d; /// degree
+        long a;
+        unsigned d;
 
         constexpr Monom(long a = 0, unsigned d = 0)
-        : a(a), d(d) { }
+        : a(a), d(d)
+        { }
 
         constexpr Monom operator-() const
         {
             return Monom(-a, d);
         }
+
         constexpr friend Monom operator*(Monom const & m, Monom const & n)
         {
             return Monom(m.a * n.a, m.d + n.d);
         }
-        
+
         friend ostream & operator<<(ostream & stream, Monom const & m)
         {
             if (m.a == 0) return stream << '0';
             if (m.a == -1) stream << '-';
             else if (m.a !=  1 || m.d == 0) stream << m.a;
             if (m.d > 0) stream << 'x';
+// #ifdef _unicode_exponents
+            // static const wchar_t powers[] = L"\x2070\x00B9\x00B2\x00B3\x2074\x2075\x2076\x2077\x2078\x2079";
+            // if (m.d > 1) stream << powers[m.d];
+// #else
             if (m.d > 1) stream << '^' << m.d;
+// #endif
             return stream;
         }
     };
@@ -45,11 +52,11 @@ class Polynom
     //  does not contain monom with .a == 0
 
 public:
-    Polynom(long a, unsigned d = 0)
+    Polynom(long a = 0, unsigned d = 0)
     : monoms(a == 0 ? 0 : 1, Monom(a, d))
     { }
 
-    Polynom(Monom const & monom = Monom())
+    Polynom(Monom const & monom)
     : monoms(monom.a == 0 ? 0 : 1, monom)
     { }
 
@@ -58,6 +65,7 @@ public:
         for (auto & monom : p.monoms) monom = -monom;
         return p;
     }
+
     friend Polynom operator+(Polynom p, Polynom q) // copy p, q
     {
         p.monoms.merge(q.monoms, [](Monom m, Monom n) { return m.d < n.d; });
@@ -78,7 +86,7 @@ public:
         return p;
     }
 
-    inline friend Polynom operator-(const Polynom & p, const Polynom & q)
+    friend Polynom operator-(const Polynom & p, const Polynom & q)
     {
         return p + (-q);
     }
@@ -88,37 +96,11 @@ public:
         Polynom result;
         for (auto const & m : p.monoms)
         for (auto const & n : q.monoms)
-            result += m * n;
+            result += m * n; // optimized
         return result;
     }
 
-    // optimized add for monoms used in operator*
-    Polynom & operator+=(Monom m)
-    {
-        // place m in a list to track it with an iterator
-        list<Monom> temp = { m };
-        auto it = temp.begin(); // track m with iterator
-        monoms.merge(temp, [](Monom m, Monom n) { return m.d < n.d; });
-        // merge will place m behind equivalent monoms.
-        // the invariants of the list enforce:
-        // if there is a monom equivalent to m, it is directly before it.
-
-        // if m is the first in the merged list, no equivalent monoms exist.
-        if (it-- == monoms.begin()) return *this;
-        // if the predecessor of m is equivalent to it ...
-        if (it->d == m.d)
-        {
-            // ... add the elements, and if the sum equals 0 ...
-            if ((it->a += m.a) == 0)
-                monoms.erase(it); // ... remove it from the list which puts the iterator to the next element ...
-            else
-                ++it; // ... otherwise put iterator to the next element which is m manually ...
-            monoms.erase(it); // ... and erase it.
-        }
-        return *this;
-    }
-
-    /// binäre exponentiation
+    /// binary exponentiation
     friend Polynom pow(Polynom p, unsigned n)
     {
         if (n == 0) return Polynom(1);
@@ -133,6 +115,32 @@ public:
         return result;
     }
 
+    // optimized add for monoms used in operator*
+    Polynom & operator+=(Monom m)
+    {
+        // place m in a list to track it with an iterator
+        list<Monom> temp = { m };
+        auto it = temp.begin(); // track m with iterator
+        monoms.merge(temp, [](Monom m, Monom n) { return m.d < n.d; });
+        // merge will place m behind equivalent monoms.
+        // the invariants of the list enforce:
+        // if there is a monom equivalent to m, it is directly before it.
+
+        // if m is the first in the merged list, no equivalent monoms exist, and we're done.
+        if (it-- == monoms.begin()) return *this;
+        // if the predecessor of m is equivalent to it ...
+        if (it->d == m.d)
+        {
+            // ... add the elements together, and if that sum equals 0 ...
+            if ((it->a += m.a) == 0)
+                monoms.erase(it); // ... remove it from the list, what increments the iterator to the next element ...
+            else
+                ++it; // ... otherwise increment iterator to the next element manually ...
+            monoms.erase(it); // ... and erase it.
+        }
+        return *this;
+    }
+
     Polynom & operator+=(Polynom p) { return *this = *this + p; }
     Polynom & operator-=(Polynom p) { return *this = *this - p; }
     Polynom & operator*=(Polynom p) { return *this = *this * p; }
@@ -140,10 +148,10 @@ public:
     friend ostream & operator<<(ostream & stream, Polynom const & p)
     {
         if (p.monoms.size() == 0) return stream << '0';
-        
+
         auto it = p.monoms.crbegin();
-        stream << *it++;
-        for (; it != p.monoms.crend(); ++it)
+        stream << *it;
+        for (++it; it != p.monoms.crend(); ++it)
         {
             if (it->a > 0) stream << '+';
             stream << *it;
@@ -169,45 +177,84 @@ private:
     size_t size;
     istringstream ein;
 
+    /// extracts character if it is one of the mentioned
+    /// returns ein.peek() in any case.
+#if __cplusplus > 201402L
+    template <typename ... Ts>
+    char maybe(Ts... should_be)
+    {
+        char result = ein.peek();
+        // C++17 Fold Expression
+        if ( (... || (result == should_be)) ) ein.get();
+        return result;
+    }
+#else
+    char maybe(char should_be)
+    {
+        char result = ein.peek();
+        if (result == should_be) ein.get();
+        return result;
+    }
+    char maybe(char should_be1, char should_be2)
+    {
+        char result = ein.peek();
+        if (result == should_be1 || result == should_be2) ein.get();
+        return result;
+    }
+#endif
+    /// if ein.peek() != expected_char, throw exception with appropriate message
+    void expect(char expected_char) // not const because of peek()
+    {
+        string message = "'_' expected";
+        message[1] = expected_char; // replace _ with c.
+        expect(expected_char, message);
+    }
+
+    /// if ein.peek() != expected_char, throw exception with given message
+    void expect(char expected_char, string message) // not const because of peek()
+    {
+        if (ein.peek() != expected_char) throw parse_error(message, pos());
+        ein.get();
+    }
+
     /// expression interpretation from ein, i.e. a + and - separated sequence of terms
     Polynom expression()
     {
         Polynom sum;
-        switch (ein.peek())
+        switch (maybe('+', '-'))
         {
-            case '+': ein.get(); // fallthrough
-            default: sum =  term(); break;
-            case '-': ein.get(); sum = -term(); break;
+            case '+': // fallthrough
+            default: sum = term(); break;
+            case '-': sum = -term();
         }
 
-        while (ein.peek() == '+' || ein.peek() == '-')
-        switch (ein.peek())
+        for (;;)
+        switch (maybe('+', '-'))
         {
-            case '+': ein.get(); sum += term(); break;
-            case '-': ein.get(); sum -= term(); break;
+            case '+': sum += term(); break;
+            case '-': sum -= term(); break;
+            default: return sum;
         }
-
-        return sum;
     }
 
     /// term interpretation from ein, i.e. a * spearated sequence of factors
     Polynom term()
     {
         Polynom prod = factor();
-        while (ein.peek() == '*')
+        for (;;)
+        switch (maybe('*'))
         {
-            ein.get();
-            prod *= factor();
+            case '*': prod *= factor(); break;
+            default : return prod;
         }
-        return prod;
     }
 
+    /// factor interpretation from ein, i.e. an operand optionally followed by '^' and non-negative integer
     Polynom factor()
     {
         Polynom result = operand();
-        if (ein.peek() != '^') return result;
-        ein.get();
-        long n;
+        if (maybe('^') != '^') return result;
+        unsigned n;
         ein >> n;
         return pow(result, n);
     }
@@ -215,9 +262,8 @@ private:
     /// operand interpretation from ein, i.e. an unsigned integer
     Polynom operand()
     {
-        if (ein.peek() == '(')
+        if (maybe('(') == '(')
         {
-            ein.get();
             Polynom result = expression();
             expect(')');
             return result;
@@ -230,29 +276,13 @@ private:
             return result;
         }
 
-        if (ein.peek() == 'x')
-        {
-            ein.get();
-            return Polynom(1, 1);
-        }
+        if (maybe('x') == 'x') return Polynom(1, 1);
 
-        if (ein.eof())
-            throw parse_error("unexpected end of expression");
-        throw parse_error("unexpected character");
-    }
-    
-    void expect(char c) // not const because of peek()
-    {
-        string message = "'.' expected";
-        message[1] = c; // replace . with c.
-        expect(c, message);
+        throw parse_error(ein.eof()
+            ? "unexpected end of expression"
+            : "unexpected character");
     }
 
-    void expect(char c, string message) // not const because of peek()
-    {
-        if (ein.peek() != c) throw parse_error(message, pos());
-        ein.get();
-    }
 
 public:
     Expression(string s) : size(s.size()), ein(s) { }
@@ -281,11 +311,11 @@ int main()
         Polynom result = eval(expr);
         cout << "Result: " << result << endl;
     }
-    catch (runtime_error& error)
+    catch (parse_error& error)
     {
         cout << "Error:" << endl;
         cout << s << endl;
-        for (int i = 0; i < expr.pos(); ++i) cout << ' ';
+        for (unsigned i = 0; i < expr.pos(); ++i) cout << ' ';
         cout << "^-- " << error.what() << endl;
     }
 }

06572c876d1c2d400cf14e250018777f501c89fd
Q. F. Schroll <qs.il.paperinik@gmail.com>
2018-11-28 04:32:29 +0100

    revise structure

--- Prog2/WS18/blatt05.cpp
+++ Prog2/WS18/blatt05.cpp
@@ -15,12 +15,13 @@ class checked
     constexpr checked(T value = T()) : value(value) { }
     constexpr operator T() const { return value; }
 
+    checked operator+() const { return *this; }
     checked operator-() const
     {
         if (std::numeric_limits<T>::is_signed && std::numeric_limits<T>::min() + std::numeric_limits<T>::max() < T(0))
         if (value == std::numeric_limits<T>::min())
         {
-            std::overflow_error("operator-: arithemtic overflow on negation");
+            throw std::overflow_error("operator-: arithmetic overflow on negation");
         }
         return checked(-value);
     }
@@ -31,16 +32,11 @@ class checked
     template<typename S> checked& operator/=(S rhs) { value = *this / rhs; return *this; }
     template<typename S> checked& operator%=(S rhs) { value = *this % rhs; return *this; }
 
-    template<typename T1, typename T2>
-    friend auto operator+(checked<T1> lhs, checked<T2> rhs);
-    template<typename T1, typename T2>
-    friend auto operator-(checked<T1> lhs, checked<T2> rhs);
-    template<typename T1, typename T2>
-    friend auto operator*(checked<T1> lhs, checked<T2> rhs);
-    template<typename T1, typename T2>
-    friend auto operator/(checked<T1> lhs, checked<T2> rhs);
-    template<typename T1, typename T2>
-    friend auto operator%(checked<T1> lhs, checked<T2> rhs);
+    template<typename T1, typename T2> friend auto operator+(checked<T1> lhs, checked<T2> rhs);
+    template<typename T1, typename T2> friend auto operator-(checked<T1> lhs, checked<T2> rhs);
+    template<typename T1, typename T2> friend auto operator*(checked<T1> lhs, checked<T2> rhs);
+    template<typename T1, typename T2> friend auto operator/(checked<T1> lhs, checked<T2> rhs);
+    template<typename T1, typename T2> friend auto operator%(checked<T1> lhs, checked<T2> rhs);
 
     friend std::ostream& operator<<(std::ostream& stream, checked   c) { return stream << c.value; }
     friend std::istream& operator>>(std::istream& stream, checked & c) { return stream >> c.value; }
@@ -80,10 +76,10 @@ template<typename T1, typename T2>
 auto operator+(checked<T1> lhs, checked<T2> rhs)
 {
     using Common = decltype(lhs.value + rhs.value);
-    if (rhs.value > T1(0) && std::numeric_limits<Common>::max() - rhs.value < lhs.value
-     || rhs.value < T1(0) && std::numeric_limits<Common>::min() - rhs.value > lhs.value)
+    if (rhs.value > T2(0) && std::numeric_limits<Common>::max() - rhs.value < lhs.value
+     || rhs.value < T2(0) && std::numeric_limits<Common>::min() - rhs.value > lhs.value)
     {
-        throw std::overflow_error("operator+: arithemtic overflow");
+        throw std::overflow_error("operator+: arithmetic overflow");
     }
     return checked<Common>(lhs.value + rhs.value);
 }
@@ -92,10 +88,10 @@ template<typename T1, typename T2>
 auto operator-(checked<T1> lhs, checked<T2> rhs)
 {
     using Common = decltype(lhs.value - rhs.value);
-    if (rhs.value < T1(0) && std::numeric_limits<Common>::max() + rhs.value < lhs.value
-     || rhs.value > T1(0) && std::numeric_limits<Common>::min() + rhs.value > lhs.value)
+    if (rhs.value < T2(0) && std::numeric_limits<Common>::max() + rhs.value < lhs.value
+     || rhs.value > T2(0) && std::numeric_limits<Common>::min() + rhs.value > lhs.value)
     {
-        throw std::overflow_error("operator-: arithemtic overflow");
+        throw std::overflow_error("operator-: arithmetic overflow");
     }
     return checked<Common>(lhs.value - rhs.value);
 }
@@ -108,7 +104,7 @@ auto operator*(checked<T1> lhs, checked<T2> rhs)
             lhs.value == T1(-1) && rhs.value == std::numeric_limits<Common>::min()
          || rhs.value == T2(-1) && lhs.value == std::numeric_limits<Common>::min()))
     {
-        throw std::overflow_error("operator*: arithemtic overflow on negation");
+        throw std::overflow_error("operator*: arithmetic overflow on negation");
     }
     if (rhs < 0 && lhs < 0)
     {
@@ -124,7 +120,7 @@ auto operator*(checked<T1> lhs, checked<T2> rhs)
         || rhs.value < std::numeric_limits<Common>::min() / lhs.value)
     )
     {
-        throw std::overflow_error("operator*: arithemtic overflow");
+        throw std::overflow_error("operator*: arithmetic overflow");
     }
     return checked<Common>(lhs.value * rhs.value);
 }
@@ -137,7 +133,7 @@ auto operator/(checked<T1> lhs, checked<T2> rhs)
     if (std::numeric_limits<Common>::is_signed && std::numeric_limits<Common>::min() + std::numeric_limits<Common>::max() < Common(0) &&
         std::numeric_limits<T2>::is_signed && rhs == T2(-1) && lhs.value == std::numeric_limits<Common>::min())
     {
-        throw std::overflow_error("operator/: arithemtic overflow on negation");
+        throw std::overflow_error("operator/: arithmetic overflow on negation");
     }
     return checked<Common>(lhs.value / rhs.value);
 }
@@ -149,6 +145,9 @@ auto operator%(checked<T1> lhs, checked<T2> rhs)
     using Common = decltype(lhs.value % rhs.value);
     return checked<Common>(lhs.value % rhs.value);
 }
+
+template<typename T>
+checked<T> check(T value) { return checked<T>(value); }
 
 }
 
@@ -169,6 +168,7 @@ int main()
 using namespace std;
 using namespace checked_int;
 
+// for debugging.
 #define READ(x) do { std::cerr << #x << ": "; std::cin >> (x); } while (0)
 #define INSPECT(x) do { std::cerr << #x << " = " << (x) << std::endl; } while (0)
 #define EXPECT(x, r) do { std::cerr << "is " << #x << " = " << (x) << " equal to " << (r) << " = " << #r << "?" << std::endl; } while (0)
@@ -180,15 +180,12 @@ class rational
     checked<T> p, q;
     // T p, q;
 
-    static T ggt(T a, T b)
+    static T gcd(T a, T b)
     {
-        if (a < 0)
-        {
-            return ggt(-checked<T>(a), b);
-        }
-        // if (b < 0) return ggt(a, -b);
+        if (a < 0) return gcd(-check(a), b);
+        if (b < 0) return gcd(a, -check(b));
         if (b == 0) return a;
-        return ggt(b, a % b);
+        return gcd(b, a % b);
     }
 
   public:
@@ -198,10 +195,10 @@ class rational
         if (q == 0) throw domain_error("denominator must not be zero");
         if (q < 0)
         {
-            p = -checked<T>(p);
-            q = -checked<T>(q);
+            p = -check(p);
+            q = -check(q);
         }
-        T g = ggt(p, q);
+        T g = gcd(p, q);
         p /= g;
         q /= g;
     }
@@ -209,7 +206,7 @@ class rational
     explicit operator long double() const { return static_cast<long double>(p) / static_cast<long double>(q); }
 
     friend rational operator+(rational r) { return r; }
-    friend rational operator-(rational r) { return rational(-r.p, r.q); }
+    friend rational operator-(rational r) { r.p = -r.p; return r; }
 
     rational& operator+=(rational r) { return *this = *this + r; }
     rational& operator-=(rational r) { return *this = *this - r; }
@@ -219,14 +216,14 @@ class rational
 
     friend rational operator+(rational s, rational t)
     {
-        T g = ggt(s.q, t.q);
+        T g = gcd(s.q, t.q);
         T k = s.q / g * t.q;
         T p = s.p * (t.q / g) + t.p * (s.q / g);
         return rational(p, k);
     }
     friend rational operator-(rational s, rational t)
     {
-        T g = ggt(s.q, t.q);
+        T g = gcd(s.q, t.q);
         T k = (s.q / g) * t.q;
         T p = s.p * (t.q / g) - t.p * (s.q / g);
         return rational(p, k);
@@ -235,8 +232,8 @@ class rational
     {
         // strategy: swap denominators to reduce the fractions:
         //  (26/35) * (15/22) = (26/22) * (15/35) = (13/11) * (3/7)
-        // the two resulting fractions not only have coprime numerators and denominators,
-        // the numerator of one is coprime to the denominator of the other,
+        // the two resulting fractions not only have co-prime numerators and denominators,
+        // the numerator of one is co-prime to the denominator of the other,
         //  (13/11) * (3/7) = (13*3)/(11*7)
         // therefore the numerator- and denominator-wise multiplication is optimal.
         rational a(s.p, t.q), b(t.p, s.q), result;
@@ -250,12 +247,10 @@ class rational
         return s * rational(t.q, t.p);
     }
 
-    friend rational operator%(rational s, rational t)
-    {
-        if (t == 0) throw invalid_argument("operator%: division by zero");
-        while (s > t) s -= t; // suboptimal
-        return s;
-    }
+    // note: integer division is intentional!
+    friend T whole(rational r) { return r.p / r.q; }
+    friend T div(rational s, rational t) { return whole(s / t); }
+    friend rational operator%(rational s, rational t) { return s - div(s, t) * t; }
 
     /*
     // recursive version
@@ -303,38 +298,33 @@ class rational
     friend bool operator< (rational s, rational t)
     {
         // trivial criteria
-        if (s.q == t.q) return s.p < t.p;
-        if (s.p == t.q) return s.q > t.q;
-        if (s.p <= t.p && s.q <= t.q) return s != t;
+        if (s.p <= 0 && 0 <= t.p) return s != t; // makes comparisons to 0 fast.
+        if (s.p <= t.p && s.q >= t.q) return s != t;
 
         // We'll just use the fact that s.p/s.q < t.p/t.q iff s.p * t.q < t.p * s.q
         // as denominators are always positive.
         // For optimization, we use the fact that for (s.p/s.q) < (t.p/t.q), we can
-        // divide both sides by (ggt(s.p, t.p)/ggt(s.q, t.q)) easily:
+        // divide both sides by (gcd(s.p, t.p)/gcd(s.q, t.q)) easily:
         //      15/22 < 35/26  using / (5/2)
         //  <=>  5/11 <  7/13  using cross multiply, i.e. * (11*13)
         //  <=>  5*13 <  7*11
         //  <=>  65   <  77
-        auto gp = ggt(s.p, t.p), gq = ggt(t.q, s.q);
-        return checked<long>(s.p / gp) * (t.q / gq) < checked<long>(t.p / gp) * (s.q / gq);
+        auto gp = gcd(s.p, t.p), gq = gcd(t.q, s.q);
+        return (s.p / gp) * (t.q / gq) < (t.p / gp) * (s.q / gq);
     }
 
     friend ostream& operator<<(ostream& stream, rational const & r)
     {
+        if (r.q == 1) return stream << r.p;
         return stream << reinterpret_cast<ostringstream&>(ostringstream() << r.p << '/' << r.q).str();
     }
-    friend istream& operator>>(istream& stream, rational& r)
+    friend istream& operator>>(istream& stream, rational & r)
     {
         T p;
         if (!(stream >> p)) return stream;
-        if (isspace(stream.peek()))
-        {
-            r = rational(p);
-            return stream;
-        }
         if (stream.peek() != '/')
         {
-            stream.setstate(ios::failbit);
+            r = rational(p);
             return stream;
         }
         stream.get();
@@ -365,8 +355,12 @@ rational<T> continued_fraction(vector<T> const & a, vector<T> const & b)
     if (a.size() != b.size() - 1u) throw invalid_argument("continued_fraction: vector sizes mismatch");
 
     rational<T> result = b.back();
-    for (auto a_it = a.crbegin(), b_it = b.crbegin() + 1; a_it != a.crend() && b_it != b.crend(); ++a_it, ++b_it)
-    {
+    for
+    (
+        auto a_it = a.crbegin(), b_it = b.crbegin() + 1;
+        a_it != a.crend() && b_it != b.crend();
+        ++a_it, ++b_it
+    ) {
         result = *b_it + *a_it / result;
     }
     return result;
@@ -411,6 +405,19 @@ int main()
         // }
         // INSPECT(pow(s, 5));
         // EXPECT(pow(s, 5) / pow(s, 4), s);
+
+        using rat = rational<long>;
+        // rat s(3, 4), t(1, 2);
+        // INSPECT_RAT(s % t);
+        for (rat a : { rat(+10, 3), rat(-10, 3) })
+        for (rat b : { rat(+3, 2), rat(-3, 2) })
+        {
+            INSPECT(a);
+            INSPECT(b);
+            INSPECT(div(a, b));
+            INSPECT(a % b);
+            EXPECT(a - (div(a, b) * b + (a % b)), 0);
+        }
     }
     catch (overflow_error e)
     {
@@ -420,11 +427,28 @@ int main()
 }
 */
 
+// for debugging vectors
+ostream& operator<<(ostream& stream, vector<int> const & v)
+{
+    stream << '[';
+    if (!v.empty())
+    {
+        stream << v[0];
+        for (vector<int>::size_type i = 1; i < v.size(); ++i)
+            stream << ", " << v[i];
+    }
+    stream << ']';
+    return stream;
+}
+
+long double diff(long double a, long double b) { return a < b ? b - a : a - b; }
+
 int main()
 {
-    using rat = rational<int_least64_t>;
-    cout << "-- (a) and (b) ----------" << endl;
-    for (rat r : { rat(2,3), rat(-10,7) })
+    using T = int_least64_t;
+    using rat = rational<T>;
+    cout << "\n-- (a) and (b) ----------" << endl;
+    for (rat r : { rat(2, 3), rat(-10, 7) })
     {
         const int n = 8;
         rat ps = power_series(r, n);
@@ -433,23 +457,34 @@ int main()
         INSPECT_RAT((n * pow(r, n + 2) - (n + 1) * pow(r, n + 1) + r) / pow(r - 1, 2));
     }
 
-    cout << "-- (c) ------------------" << endl;
-    INSPECT_RAT(continued_fraction(vector<int>{ 3, 7, 15, 1, 292 }));
-    
-    cout << "-- (d) ------------------" << endl;
-    for (int n = 1; n < 11; ++n)
+    cout << "\n-- (c) ------------------" << endl;
+    using vec = vector<T>;
+    INSPECT_RAT(continued_fraction(vec{ 3, 7, 15, 1, 292 }));
+
+    cout << "\n-- (d) ------------------" << endl;
+    const long double pi = 3.141592653589793238462643383279L;
+    for (int n = 1; n < 25; ++n)
     {
-        vector<int> a(n - 1), b(n);
-        if (a.size() > 0u) a[0] = 4;
-        b[0] = 0;
-        for (int i = 1; i < n; ++i)
+        try
         {
-            a[i] = i * i;
-            b[i] = 2 * i - 1;
+            vector<T> a(n, 4);
+            vector<T> b(n + 1, 0);
+            for (int i = 1; i < n; ++i)
+            {
+                a[i] = i * i;
+                b[i] = 2 * i - 1;
+            }
+            b[n] = 2 * n - 1;
+
+            auto r = continued_fraction(a, b);
+            long double lr = static_cast<long double>(r);
+            cout << "for n = " << setw(2) << n << ": ";
+            cout << "continued_fraction(a, b) = " << setw(19) << fixed << setprecision(numeric_limits<long double>::digits10) << lr << " (diff = " << diff(lr, pi) << ")";
+            cout << " = " << r << endl;
         }
-        auto r = continued_fraction(a, b);
-        cout << "for n = " << setw(2) << n << ": ";
-        cout << "continued_fraction(a, b) = " << setw(15) << r;
-        cout << " = " << setw(19) << fixed << setprecision(numeric_limits<long double>::digits10) << static_cast<long double>(r) << endl;
+        catch (overflow_error)
+        {
+            cout << "for n = " << setw(2) << n << ": *** overflow ***" << endl;
+        }
     }
 }
--- Prog2/WS18/blatt05_entwurf.cpp
+++ Prog2/WS18/blatt05_entwurf.cpp
@@ -12,15 +12,16 @@ class checked
 {
     T value;
   public:
-    constexpr checked(T value = T()) : value(value) { }
-    constexpr operator T() const { return value; }
+    checked(T value = T()) : value(value) { }
+    operator T() const { return value; }
 
+    checked operator+() const { return *this; }
     checked operator-() const
     {
         if (std::numeric_limits<T>::is_signed && std::numeric_limits<T>::min() + std::numeric_limits<T>::max() < T(0))
         if (value == std::numeric_limits<T>::min())
         {
-            std::overflow_error("operator-: arithemtic overflow on negation");
+            throw std::overflow_error("operator-: arithmetic overflow on negation");
         }
         return checked(-value);
     }
@@ -31,16 +32,11 @@ class checked
     template<typename S> checked& operator/=(S rhs) { value = *this / rhs; return *this; }
     template<typename S> checked& operator%=(S rhs) { value = *this % rhs; return *this; }
 
-    template<typename T1, typename T2>
-    friend auto operator+(checked<T1> lhs, checked<T2> rhs) -> checked<decltype(lhs.value + rhs.value)>;
-    template<typename T1, typename T2>
-    friend auto operator-(checked<T1> lhs, checked<T2> rhs) -> checked<decltype(lhs.value - rhs.value)>;
-    template<typename T1, typename T2>
-    friend auto operator*(checked<T1> lhs, checked<T2> rhs) -> checked<decltype(lhs.value * rhs.value)>;
-    template<typename T1, typename T2>
-    friend auto operator/(checked<T1> lhs, checked<T2> rhs) -> checked<decltype(lhs.value / rhs.value)>;
-    template<typename T1, typename T2>
-    friend auto operator%(checked<T1> lhs, checked<T2> rhs) -> checked<decltype(lhs.value % rhs.value)>;
+    template<typename T1, typename T2> friend auto operator+(checked<T1> lhs, checked<T2> rhs);
+    template<typename T1, typename T2> friend auto operator-(checked<T1> lhs, checked<T2> rhs);
+    template<typename T1, typename T2> friend auto operator*(checked<T1> lhs, checked<T2> rhs);
+    template<typename T1, typename T2> friend auto operator/(checked<T1> lhs, checked<T2> rhs);
+    template<typename T1, typename T2> friend auto operator%(checked<T1> lhs, checked<T2> rhs);
 
     friend std::ostream& operator<<(std::ostream& stream, checked   c) { return stream << c.value; }
     friend std::istream& operator>>(std::istream& stream, checked & c) { return stream >> c.value; }
@@ -80,10 +76,10 @@ template<typename T1, typename T2>
 auto operator+(checked<T1> lhs, checked<T2> rhs)
 {
     using Common = decltype(lhs.value + rhs.value);
-    if (rhs.value > T1(0) && std::numeric_limits<Common>::max() - rhs.value < lhs.value
-     || rhs.value < T1(0) && std::numeric_limits<Common>::min() - rhs.value > lhs.value)
+    if (rhs.value > T2(0) && std::numeric_limits<Common>::max() - rhs.value < lhs.value
+     || rhs.value < T2(0) && std::numeric_limits<Common>::min() - rhs.value > lhs.value)
     {
-        throw std::overflow_error("operator+: arithemtic overflow");
+        throw std::overflow_error("operator+: arithmetic overflow");
     }
     return checked<Common>(lhs.value + rhs.value);
 }
@@ -92,10 +88,10 @@ template<typename T1, typename T2>
 auto operator-(checked<T1> lhs, checked<T2> rhs)
 {
     using Common = decltype(lhs.value - rhs.value);
-    if (rhs.value < T1(0) && std::numeric_limits<Common>::max() + rhs.value < lhs.value
-     || rhs.value > T1(0) && std::numeric_limits<Common>::min() + rhs.value > lhs.value)
+    if (rhs.value < T2(0) && std::numeric_limits<Common>::max() + rhs.value < lhs.value
+     || rhs.value > T2(0) && std::numeric_limits<Common>::min() + rhs.value > lhs.value)
     {
-        throw std::overflow_error("operator-: arithemtic overflow");
+        throw std::overflow_error("operator-: arithmetic overflow");
     }
     return checked<Common>(lhs.value - rhs.value);
 }
@@ -108,7 +104,7 @@ auto operator*(checked<T1> lhs, checked<T2> rhs)
             lhs.value == T1(-1) && rhs.value == std::numeric_limits<Common>::min()
          || rhs.value == T2(-1) && lhs.value == std::numeric_limits<Common>::min()))
     {
-        throw std::overflow_error("operator*: arithemtic overflow on negation");
+        throw std::overflow_error("operator*: arithmetic overflow on negation");
     }
     if (rhs < 0 && lhs < 0)
     {
@@ -124,7 +120,7 @@ auto operator*(checked<T1> lhs, checked<T2> rhs)
         || rhs.value < std::numeric_limits<Common>::min() / lhs.value)
     )
     {
-        throw std::overflow_error("operator*: arithemtic overflow");
+        throw std::overflow_error("operator*: arithmetic overflow");
     }
     return checked<Common>(lhs.value * rhs.value);
 }
@@ -137,7 +133,7 @@ auto operator/(checked<T1> lhs, checked<T2> rhs)
     if (std::numeric_limits<Common>::is_signed && std::numeric_limits<Common>::min() + std::numeric_limits<Common>::max() < Common(0) &&
         std::numeric_limits<T2>::is_signed && rhs == T2(-1) && lhs.value == std::numeric_limits<Common>::min())
     {
-        throw std::overflow_error("operator/: arithemtic overflow on negation");
+        throw std::overflow_error("operator/: arithmetic overflow on negation");
     }
     return checked<Common>(lhs.value / rhs.value);
 }
@@ -150,7 +146,20 @@ auto operator%(checked<T1> lhs, checked<T2> rhs)
     return checked<Common>(lhs.value % rhs.value);
 }
 
+template<typename T>
+checked<T> check(T value) { return checked<T>(value); }
+
 }
+
+#define STRINGIZE_DETAIL(x) #x
+#define STRINGIZE(x) STRINGIZE_DETAIL(x)
+
+struct todo_missing : public std::logic_error { todo_missing(const char * p) : std::logic_error(p) { } };
+
+#define TODO() throw todo_missing("something not implemented in line " STRINGIZE(__LINE__) ".")
+
+
+// ***** BEGIN HERE *****
 
 using namespace std;
 using namespace checked_int;
@@ -159,11 +168,14 @@ class rational
 {
   private:
     checked<long> p, q;
-    // T p, q;
 
-    static long ggt(long a, long b)
+    // greatest common divisor
+    static long gcd(long a, long b)
     {
-        throw logic_error("not implemented");
+        if (a < 0) TODO();
+        if (b < 0) TODO();
+        if (b == 0) TODO();
+        TODO(); // the main case
     }
 
   public:
@@ -173,77 +185,77 @@ class rational
         if (q == 0) throw domain_error("denominator must not be zero");
         if (q < 0)
         {
-            throw logic_error("not implemented");
+            TODO();
         }
-        throw logic_error("not implemented");
+        TODO();
     }
 
-    explicit operator long double() const { throw logic_error("not implemented"); }
+    explicit operator long double() const { TODO(); }
 
-    friend rational operator+(rational r) { throw logic_error("not implemented"); }
-    friend rational operator-(rational r) { throw logic_error("not implemented"); }
+    friend rational operator+(rational r) { TODO(); }
+    friend rational operator-(rational r) { TODO(); }
 
-    rational& operator+=(rational r) { throw logic_error("not implemented"); }
-    rational& operator-=(rational r) { throw logic_error("not implemented"); }
-    rational& operator*=(rational r) { throw logic_error("not implemented"); }
-    rational& operator/=(rational r) { throw logic_error("not implemented"); }
-    rational& operator%=(rational r) { throw logic_error("not implemented"); }
+    rational& operator+=(rational r) { return *this = *this + r; }
+    rational& operator-=(rational r) { return *this = *this - r; }
+    rational& operator*=(rational r) { return *this = *this * r; }
+    rational& operator/=(rational r) { return *this = *this / r; }
 
     friend rational operator+(rational s, rational t)
     {
-        throw logic_error("not implemented");
+        TODO();
     }
     friend rational operator-(rational s, rational t)
     {
-        throw logic_error("not implemented");
+        TODO();
     }
     friend rational operator*(rational s, rational t)
     {
-        throw logic_error("not implemented");
+        TODO();
     }
     friend rational operator/(rational s, rational t)
     {
-        throw logic_error("not implemented");
-    }
-
-    friend rational operator%(rational s, rational t)
-    {
-        throw logic_error("not implemented");
+        if (t == 0) throw invalid_argument("operator/: division by zero");
+        TODO();
     }
 
     friend rational pow(rational b, int n)
     {
-        throw logic_error("not implemented");
+        if (n == 0) return 1;
+        if (b == 0) return b;
+        if (n < 0)
+        {
+            TODO();
+        }
+        TODO();
     }
 
-    friend bool operator==(rational s, rational t) { throw logic_error("not implemented"); }
-    friend bool operator!=(rational s, rational t) { throw logic_error("not implemented"); }
-    friend bool operator> (rational s, rational t) { throw logic_error("not implemented"); }
-    friend bool operator<=(rational s, rational t) { throw logic_error("not implemented"); }
-    friend bool operator>=(rational s, rational t) { throw logic_error("not implemented"); }
+    friend bool operator==(rational s, rational t) { return s.p == t.p && s.q == t.q; }
+    friend bool operator!=(rational s, rational t) { return !(s == t); }
+    friend bool operator> (rational s, rational t) { return t < s; }
+    friend bool operator<=(rational s, rational t) { return s == t || s < t; }
+    friend bool operator>=(rational s, rational t) { return s == t || s > t; }
     friend bool operator< (rational s, rational t)
     {
         // trivial criteria
-        if (s.q == t.q) return s.p < t.p;
-        if (s.p == t.q) return s.q > t.q;
-        if (s.p <= t.p && s.q <= t.q) return s != t;
+        if (s.p <= 0 && 0 <= t.p) return s != t; // makes comparisons to 0 fast.
+        if (s.p <= t.p && s.q >= t.q) return s != t;
 
-        throw logic_error("not implemented");
+        TODO();
     }
 
     friend ostream& operator<<(ostream& stream, rational const & r)
     {
-        throw logic_error("not implemented");
+        TODO();
     }
-    friend istream& operator>>(istream& stream, rational& r)
+    friend istream& operator>>(istream& stream, rational & r)
     {
-        throw logic_error("not implemented");
+        TODO();
     }
 };
 
 rational power_series(rational const & r, int n)
 {
-    throw logic_error("not implemented");
+    TODO();
 }
 
 rational continued_fraction(vector<long> const & a, vector<long> const & b)
@@ -251,27 +263,53 @@ rational continued_fraction(vector<long> const & a, vector<long> const & b)
     if (b.size() == 0u) throw invalid_argument("continued_fraction: vector of coefficients must not be empty");
     if (a.size() != b.size() - 1u) throw invalid_argument("continued_fraction: vector sizes mismatch");
 
-    throw logic_error("not implemented");
+    TODO();
 }
 
+// version where the a[i] default to ones.
 rational continued_fraction(vector<long> const & b)
 {
     if (b.size() == 0u) throw invalid_argument("continued_fraction: vector of coefficients must not be empty");
-    throw logic_error("not implemented");
+    return continued_fraction(vector<long>(b.size() - 1, 1), b);
 }
+
+// for debugging vectors of int; prints [v0, v1, .., vn]
+// comment in if you need it.
+/*
+ostream& operator<<(ostream& stream, vector<int> const & v)
+{
+    stream << '[';
+    if (!v.empty())
+    {
+        stream << v[0];
+        for (vector<int>::size_type i = 1; i < v.size(); ++i)
+            stream << ", " << v[i];
+    }
+    stream << ']';
+    return stream;
+}
+*/
+
 
 int main()
 {
-    cout << "-- (a) and (b) ----------" << endl;
-    for (rational r : { rational(2,3), rational(-10,7) })
+    try
     {
-        const int n = 8;
-        throw logic_error("not implemented");
-    }
+        cout << "\n-- (a) and (b) ----------" << endl;
+        for (rational r : { rational(2, 3), rational(-10, 7) })
+        {
+            const int n = 8;
+            // TODO
+        }
 
-    cout << "-- (c) ------------------" << endl;
-    throw logic_error("not implemented");
-    
-    cout << "-- (d) ------------------" << endl;
-    throw logic_error("not implemented");
+        cout << "\n-- (c) ------------------" << endl;
+        // TODO
+
+        cout << "\n-- (d) ------------------" << endl;
+        // TODO
+    }
+    catch (todo_missing const & e)
+    {
+        cerr << e.what() << endl;
+    }
 }
